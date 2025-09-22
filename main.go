a524be2ceb90645cbb7b7d145c8e822160d5453d
Bruno <loiaconobruno08@gmail.com>
2025-09-22 19:55:43 -0300

    Setting => (vendor/ && go.mod && go.sum && main.go): Installing sqlc && goose

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
-
 	godotenv.Load()
 
 	portString := os.Getenv("PORT")
@@ -20,4 +21,43 @@ func main() {
 	} else {
 		fmt.Println("Port: ", portString)
 	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://example.com", "http://localhost:3000"},
+
+		AllowedMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+
+		AllowedHeaders: []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+			"X-CSRF-Token",
+		},
+
+		ExposedHeaders: []string{"Link"},
+
+		AllowCredentials: true,
+
+		MaxAge: 300, // 5 minutos
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handleErr)
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port: ", portString)
 }
