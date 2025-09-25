40526003e7c485c0650b4346b27ede6c4a20ecdb
Bruno <loiaconobruno08@gmail.com>
2025-09-25 19:34:57 -0300

    FIX => (fixing Bugs)

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -7,38 +7,18 @@ package database
 
 import (
 	"context"
-	"database/sql"
-
-	"github.com/google/uuid"
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (
-  id, name, email, created_at
-) VALUES ( $1, $2, $3, $4)
-RETURNING id, name, email, created_at
+CREATE EXTENSION IF NOT EXISTS pgcrypto
 `
 
-type CreateUserParams struct {
-	ID        uuid.UUID
-	Name      string
-	Email     string
-	CreatedAt sql.NullTime
+type CreateUserRow struct {
 }
 
-func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, error) {
-	row := q.db.QueryRowContext(ctx, createUser,
-		arg.ID,
-		arg.Name,
-		arg.Email,
-		arg.CreatedAt,
-	)
-	var i User
-	err := row.Scan(
-		&i.ID,
-		&i.Name,
-		&i.Email,
-		&i.CreatedAt,
-	)
+func (q *Queries) CreateUser(ctx context.Context) (CreateUserRow, error) {
+	row := q.db.QueryRowContext(ctx, createUser)
+	var i CreateUserRow
+	err := row.Scan()
 	return i, err
 }
