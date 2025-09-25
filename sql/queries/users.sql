1940ea35ccf43b4e96c66b168d4b8718a83618b0
Bruno <loiaconobruno08@gmail.com>
2025-09-25 19:38:55 -0300

    nothing to say4

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,8 +2,7 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO users (
   name, email, created_at, api_key
-) VALUES ($1, $2, $3,
+) VALUES ($1, $2, NOW(),
   encode(gen_random_bytes(32), 'hex')
 )
 RETURNING *;
-
