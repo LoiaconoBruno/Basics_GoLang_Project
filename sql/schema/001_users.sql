cdcd065f7d5807ababc1e7a1166dfae1de7d54ab
Bruno <loiaconobruno08@gmail.com>
2025-09-24 21:30:07 -0300

    FIX => (Bug Fixed)

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,10 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT now()
 );
-
 -- +goose Down
 DROP TABLE users;
