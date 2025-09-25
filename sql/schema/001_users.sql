34fa6416229c97d670c4dd1ab867edf9934835fe
Bruno <loiaconobruno08@gmail.com>
2025-09-24 21:35:49 -0300

    nothing to say2

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -2,14 +2,11 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-<<<<<<< HEAD
-    id UUID PRIMARY KEY DEFAULT
-=======
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
->>>>>>> schema
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT now()
 );
+
 -- +goose Down
 DROP TABLE users;
